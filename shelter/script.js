81859b8735e5f6a57b4339b2b8e5619c08da26e1
AlexIv <undersundernew@gmail.com>
2024-09-14 15:47:56 +0300

    feat: add burger functionality

---
+++ shelter/script.js
@@ -0,0 +1,21 @@
+// BURGER
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
+const main = document.body.children[1];
+
+const openNav = () => {
+  nav.classList.toggle('nav-active');
+  burger.classList.toggle('burger-active');
+  main.classList.toggle('shadow-active');
+  document.body.classList.toggle('overflow');
+};
+
+const scrolByNav = () => {
+  nav.classList.remove('nav-active');
+  burger.classList.remove('burger-active');
+  main.classList.remove('shadow-active');
+  document.body.classList.remove('overflow');
+};
+
+burger.addEventListener('click', openNav);
+nav.addEventListener('click', scrolByNav);
